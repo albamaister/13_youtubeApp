c1af72eb6b21dfd608cd9000d0bcfa1b14b4aa3e
Andres Alba <bryanalba92@gmail.com>
2019-08-18 02:18:35 -0500

    Creación de la interfaz de los videos

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { YoutubeService } from '../../services/youtube.service';
 
+declare var $: any;
 
 @Component({
   selector: 'app-home',
@@ -9,12 +10,18 @@ import { YoutubeService } from '../../services/youtube.service';
 })
 export class HomeComponent implements OnInit {
   videos: any[] = [];
+  videoSel: any;
   constructor(public _ys: YoutubeService) {
     this._ys.getVideos().subscribe(videos => {
       console.log(videos);
       this.videos = videos;
 
     });
+   }
+
+   verVideo(video: any) {
+     this.videoSel = video;
+     $('#exampleModal').modal();
    }
 
   ngOnInit() {
